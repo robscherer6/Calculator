34f714db11e7da8502764602a3df9b361a5e75d4
Rob Scherer <robscherer6@gmail.com>
2022-11-01 06:42:44 -0400

    created operate function

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 function add (a, b) {
-
+  return a + b;
 }
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+function operate (operator, num1, num2) {
+  console.log(operator(num1, num2));
+}
+
+operate(divide, 12, 4)
